f9038bb0814351887c67f02dd00004c5340116e7
Jakub Abram <j.abram1@wp.pl>
2025-11-10 15:23:54 +0100

    created express app, listened to port 3000 and created 2 routes

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+app.listen(3000)
+
+app.get("/", (req, res) => {
+    res.contentType("text/html")
+    res.sendFile("views/index.html", {root: __dirname})
+    //res.send("<p>home page</p>")
+})
+
+app.get("/about", (req, res) => {
+    res.contentType("text/html")
+    res.sendFile("views/about.html", {root: __dirname})
+})
