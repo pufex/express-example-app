afbea960ea219daf71a7078ad72f7017f801c0e9
Jakub Abram <j.abram1@wp.pl>
2025-11-10 15:31:14 +0100

    added link to the homepage in the 404 page

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.get("/about", (req, res) => {
     res.sendFile("views/about.html", {root: __dirname})
 })
 
+//redirect
 app.get("/about-us", (req,res) => {
     res.redirect("/about")
+})
+
+
+//404
+app.use((req, res) => {
+    res.sendFile("views/404.html", {root: __dirname})
+    
 })
